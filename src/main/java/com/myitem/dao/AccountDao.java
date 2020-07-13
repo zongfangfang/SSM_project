4b0462c5c6af7f61ee6093661562e9d9b86d270e
zongfangfang <1047670763@qq.com>
2020-07-13 22:17:01 +0800

    annoation注解

--- src/main/java/com/myitem/dao/AccountDao.java
+++ src/main/java/com/myitem/dao/AccountDao.java
@@ -1,6 +1,11 @@
 package com.myitem.dao;
 
 import com.myitem.domain.Account;
+import org.apache.ibatis.annotations.One;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.mapping.FetchType;
 
 import java.util.List;
 
@@ -10,11 +15,20 @@ public interface AccountDao{
      *
      * @return
      */
-    List<Account> findAccount();
+    @Select("select * from account where uid=#{uid}")
+    List<Account> findAccountByUid(Integer uid);
 
     /**
      * 一个账号只属于一个用户
      * @return
      */
+    @Select("select * from account ")
+    @Results(id="accountMap",value = {
+            @Result(id=true,column = "id",property = "id"),
+            @Result(property="uid", column="uid"),
+            @Result(property="money", column="money"),
+            @Result(property = "user",column = "uid",
+                    one=@One(select="com.myitem.dao.UserDao.findUserById",fetchType= FetchType.EAGER))
+    })
     List<Account> findAllAccount();
 }
--- src/main/java/com/myitem/dao/UserDao.java
+++ src/main/java/com/myitem/dao/UserDao.java
@@ -3,34 +3,47 @@ package com.myitem.dao;
 import com.myitem.domain.QueryVo;
 import com.myitem.domain.User;
 import com.myitem.domain.UserAccount;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.mapping.FetchType;
 
 import java.util.List;
-
+//使用二级缓存
+@CacheNamespace(blocking = false)
 public interface UserDao {
     /**
      * 查询所有用户信息，并且查询一个用户下所有的账号
      * @return
      */
-//    @Select("select * from user")
+    @Select("select * from user")
+    @Results(id="userMap",value={
+            @Result(id=true,column = "id",property = "id"),
+            @Result(property="username", column="username"),
+            @Result(property="address", column="address"),
+            @Result(property="sex", column="sex"),
+            @Result(property="birthday", column="birthday")
+            @Result(property = "accountList", column = "id",many = @Many(select="com.myitem.dao.findAccountByUid",fetchType = FetchType.LAZY))
+    })
     List<UserAccount> findAll();
 
     /**
      * 保存用户属性
      * @param user
      */
+    @Insert(" insert into user(username,address,sex,birthday) values (#{username},#{address},#{sex},#{birthday})")
     void saveUser(User user);
 
     /**
      * 根据ID删除用户
      * @param id
      */
+    @Delete("delete from user where id=#{id}")
     void deleteUser(Integer id);
 
     /**
      * 更新用户信息
      * @param user
      */
+    @Update("update user set username=#{username},address=#{address} where id=#{id}")
     void updateUser(User user);
 
     /**
@@ -38,13 +51,18 @@ public interface UserDao {
      * @param id
      * @return
      */
+    @Select("select * from user where id=#{id}")
+    @ResultMap(value = {"userMap"})
     User findUserById(Integer id);
 
     /**
      * 模糊查询用户信息
+     * 测试类中不要加%
      * @param name
      * @return
      */
+    @Select("select * from user where username like '%${value}%'")
+    @ResultMap(value = {"userMap"})
     List<User> findUserByName(String name);
 
     /**
